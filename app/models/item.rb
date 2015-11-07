18a89f7218ec11aa9a77df830b69639512c0e0bb
Eduardo Figarola <eduardo.figarola@magmalabs.io>
2015-11-07 10:48:55 -0600

    Add associations on models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ActiveRecord::Base
+  has_many   :comments
+  has_one    :image
+  belongs_to :user
+  belongs_to :voting
 end
--- app/models/voting.rb
+++ app/models/voting.rb
@@ -1,2 +1,3 @@
 class Voting < ActiveRecord::Base
+  has_many :items
 end
