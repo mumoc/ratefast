4e5ad5103615531881d1703c130151a6ddf4362b
Any Venlz <ventura.802@hotmail.com>
2016-02-05 10:09:57 -0600

    Add formatting for item spec and change several expect

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,23 +7,26 @@ describe Item do
   describe 'fixed' do
     it "return a item with field fixed is equal to true" do
       item.update_attributes(fixed: true)
-      expect(Item.fixed(item.voting_id).count).to eq(1)
+      expect(Item.fixed(item.voting_id).first).to eq(item)
     end
   end
+
   describe 'special' do
     it "return a item with field special is equal to true" do
       item.update_attributes(special: true)
-      expect(Item.special(item.voting_id).count).to eq(1)
+      expect(Item.special(item.voting_id).first).to eq(item)
     end
   end
+
   describe 'most_voted' do
     it "return most_voted item were fixed and special is equal to false" do
-      expect(Item.most_voted(item.voting_id).count).to eq(1)
+      expect(Item.most_voted(item.voting_id).first).to eq(item)
     end
   end
+
   describe 'for_results' do
     it "return results of fixed, special and most voted methods" do
-      expect(Item.for_results(item.voting_id).count).to eq(1)
+      expect(Item.for_results(item.voting_id).first).to eq(item)
     end
   end
 end
