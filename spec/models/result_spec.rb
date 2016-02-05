a72d1f75197007ac31b30ed21d0034d53f1c2b75
Any Venlz <ventura.802@hotmail.com>
2016-02-05 10:10:24 -0600

    Add formatting for result spec and change several expect

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -11,6 +11,7 @@ describe Result do
       expect(Result.update_results_date(days)).to be_nil
     end
   end
+
   describe 'find_date' do
     it "find the date of days hash" do
       index = [result.id, "0"]
@@ -19,11 +20,13 @@ describe Result do
       expect(Result.find_date(index[1])).to eq(date + delta)
     end
   end
+
   describe 'by_voting' do
     it "return the results by voting and in ASC form" do
       expect(Result.by_voting(result.voting_id)).not_to be_empty
     end
   end
+
   describe 'day' do
     it "return the week day that match with numbers 0 to 4, if number is 0 return Monday" do
       expect(Result.day("0")).to eq("Monday")
